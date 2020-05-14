a61f426aaf9d0d87bf6cda3669c4275fcb31c897
James Schumm <j.schumm@manchester.ac.uk>
2020-05-14 17:23:57 +0100

    Add two new content pages for About, Contact

---
+++ app/Views/pages/about.php
@@ -0,0 +1,4 @@
+<div id="about">  
+        <h1>About</h1>  
+        <p>What a great site, really tremendous</p>  
+</div>  
---
+++ app/Views/pages/contact.php
@@ -0,0 +1,4 @@
+<div id="contact">  
+        <h1>Contact</h1>  
+        <p>OK, but keep your distance</p>  
+</div>  
