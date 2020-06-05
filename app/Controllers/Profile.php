b40b0665c2082c5c7d1d3dc3ad8b4ad9da13bda1
James Schumm <j.schumm@manchester.ac.uk>
2020-06-05 12:17:19 +0100

    Allow saving of profile data in session variables
    Profile data (favourite colour, fruit) can be saved on profile tab if valid user logged in. The fields are populated with current choice and remain so even if user leaves page (tab) & revisits. If the Profile page is visited by user not validly logged in then the page tells them so & they are not given chance to enter profile data.

--- app/Controllers/Profile.php
+++ app/Controllers/Profile.php
@@ -5,6 +5,13 @@ use CodeIgniter\Controller;
   
 class Profile extends Controller { 
 
+    public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
+    {
+        // Do Not Edit This Line
+        parent::initController($request, $response, $logger);
+		$this->session = \Config\Services::session();
+	}
+
 	public function display()
 	{
 		if ( ! is_file(APPPATH.'/Views/pages/profile_view.php'))
@@ -14,11 +21,52 @@ class Profile extends Controller {
 			}
 
 		$data['title'] = "Profile";  //Page title
+		
+		if (! array_key_exists('user',$_SESSION)) {
+			echo view('templates/header', $data);
+			echo view('pages/nav', $data);
+			echo "You need to login before you can enter profile data";
+			echo view('templates/footer', $data);
+			return;
+		}
+
+		
+		
+		if (array_key_exists('favcolour',$_SESSION)) {
+			$data['favcolour']=$_SESSION['favcolour'];
+		}
+		else 
+		{
+			$data['favcolour']='';
+		}
+		if (array_key_exists('favfruit',$_SESSION)) {
+			$data['favfruit']=$_SESSION['favfruit'];
+		}
+		else 
+		{
+			$data['favfruit']='';
+		}		
+			
 
 		echo view('templates/header', $data);
 		echo view('pages/nav', $data);
 		echo view('pages/profile_view', $data);
+		echo $_SESSION['user'];
 		echo view('templates/footer', $data);
 	}
-}     
+    
+
+	public function processForm()  
+    {  
+        $_SESSION['favcolour'] = $this->request->getVar('favcolour'); 
+        $_SESSION['favfruit'] = $this->request->getVar('favfruit');
+		$data['favcolour']=$_SESSION['favcolour'];		
+		$data['favfruit']=$_SESSION['favfruit'];
+		echo view('templates/header', $data);
+		echo view('pages/nav', $data);
+		echo view('pages/profile_view', $data);
+		echo view('templates/footer', $data);		
+ 
+    } 
+}	
 ?>  
--- app/Views/pages/profile_view.php
+++ app/Views/pages/profile_view.php
@@ -1,14 +1,14 @@
 <div id="profile"> 
-    <?php echo isset($error) ? $error : ''; ?>  
-    <form method="post" action="<?php echo site_url('Profile/process'); ?>"> <!-- Call Controller Profile's process method -->
+   
+    <form method="post" action="<?php echo site_url('Profile/processForm'); ?>"> <!-- Call Controller Profile's process method -->
         <table cellpadding="2" cellspacing="2">  
             <tr>  
                 <td><th>Favourite colour:</th></td>  
-                <td><input type="text" name="colour"></td>  
+                <td><input type="text" name="favcolour" value="<?php echo $favcolour;?>"></td>  
             </tr>  
             <tr>  
                 <td><th>Favourite fruit:</th></td>  
-                <td><input type="text" name="fruit"></td>  
+                <td><input type="text" name="favfruit" value="<?php echo $favfruit;?>"></td>  
             </tr>  
   
             <tr>  
